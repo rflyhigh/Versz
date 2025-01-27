d975db4cbc69da7fbe9c704605b0f2bbafe42ccf
rflyhigh <kushwahashekhar93@gmail.com>
2025-01-27 18:40:42 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+const config = {
+    backendUrl: 'https://tiffintreats.onrender.com',
+    clientId: 'edb73d1722664867b19143d6e2cba16f', 
+    scopes: [
+        'user-read-currently-playing',
+        'user-read-recently-played',
+        'user-read-playback-state'
+    ].join(' ')
+};
