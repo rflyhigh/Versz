d06c7c07c13f6b89376ab46957979179f0f8c589
rflyhigh <kushwahashekhar93@gmail.com>
2025-01-28 12:30:50 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,7 @@ const config = {
     scopes: [
         'user-read-currently-playing',
         'user-read-recently-played',
-        'user-read-playback-state'
+        'user-read-playback-state',
+        'user-top-read' 
     ].join(' ')
 };
