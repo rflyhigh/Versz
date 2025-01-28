af84edecfce47df92e3a9c8dc74305f22d2678c4
rflyhigh <kushwahashekhar93@gmail.com>
2025-01-28 16:23:37 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
-    backendUrl: 'https://tiffintreats.onrender.com',
+    backendUrl: 'https://versz.onrender.com',
     clientId: 'edb73d1722664867b19143d6e2cba16f',
     redirectUri: 'https://versz.fun/callback.html',
     scopes: [
