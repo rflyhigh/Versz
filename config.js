cd321de2af51ec81412937f83b0dbeb2fd1f6789
rflyhigh <kushwahashekhar93@gmail.com>
2025-01-30 14:42:22 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,12 +2,12 @@ const config = {
     backendUrl: 'https://versz.onrender.com',
     clientId: 'e4625fedf8a24040aa6030051efcd883',
     redirectUri: 'https://versz.fun/callback.html',
-    scopes: encodeURIComponent([
+    scopes: [
         'user-read-currently-playing',
         'user-read-recently-played',
         'user-read-playback-state',
         'user-top-read',
-        'playlist-read-collaborative',
+        'playlist-read-private',  // You might need this instead of collaborative
         'playlist-read-public'
-    ].join(' '))
+    ].join(' ')
 };
