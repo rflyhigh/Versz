7b966b2231791da63e309399b5548aa9672e967b
rflyhigh <kushwahashekhar93@gmail.com>
2025-01-30 14:35:03 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,9 @@
 const config = {
-    backendUrl: 'https://versz.onrender.com',
-    clientId: 'e4625fedf8a24040aa6030051efcd883',
-    redirectUri: 'https://versz.fun/callback.html',
     scopes: [
         'user-read-currently-playing',
         'user-read-recently-played',
-        'user-read-playback-state',
-        'user-top-read',                // Fixed scope
+        'user-read-playback-state', 
+        'user-read-top-read',
         'playlist-read-collaborative',
         'playlist-read-public'
     ].join(' ')
